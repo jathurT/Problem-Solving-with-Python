c18816893e32fd5f9386a20fa3a221458dadae89
jathurT <jathurshan_t_e23@engug.ruh.ac.lk>
2025-01-18 06:10:41 +0530

    202. Happy Number Solution Added

---
+++ Happy Number.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isHappy(self, n):
+        fast = n
+        slow = n
+        while True:
+            slow = digitSquareSum(slow)
+            fast = digitSquareSum(fast)
+            fast = digitSquareSum(fast)
+            if slow == fast:
+                break
+        return slow == 1
+
+def digitSquareSum(n):
+    sum = 0
+    while n:
+        sum += (n % 10) * (n % 10)
+        n /= 10
+    return sum
